a5f03a6a3bafdd9ac9cf3f7025320c4b1add0bed
Toyehamzat <anibiseun@gmail.com>
2024-07-03 14:20:07 +0100

    chore: Update navbar component with shadow and cursor-pointer styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   const profileName = "Deko";
   //   const profilePicUrl = "https://example.com/profile-pic.jpg";
   return (
-    <div className="bg-white py-5 px-8 relative">
+    <div className="bg-white py-5 px-8 relative shadow-md">
       <div className="flex text-center items-center justify-between">
         <div className="flex flex-row gap-10 items-center">
           <div className="text-4xl text-blue-900 font-extrabold">
@@ -15,14 +15,14 @@ function Navbar() {
         </div>
         <div className="flex items-center space-x-3">
           <div className="relative">
-            <FiBell size={24} className="text-gray-600" />
+            <FiBell size={24} className="text-gray-600 cursor-pointer" />
             <span className="absolute top-0 right-0 block h-2 w-2 rounded-full bg-red-500 ring-2 ring-white" />
           </div>
 
           <div className="flex items-center space-x-3">
-            <div className="h-10 w-10 rounded-full bg-black object-cover"></div>
+            <div className="h-10 w-10 rounded-full bg-black object-cover cursor-pointer"></div>
             <span className="font-medium text-gray-700">{profileName}</span>
-            <FiChevronDown size={20} className="text-gray-500" />
+            <FiChevronDown size={20} className="text-gray-500 cursor-pointer" />
           </div>
         </div>
       </div>
