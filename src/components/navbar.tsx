effba26e1b886219181da495bcc0dc71865ebfcf
Toyehamzat <anibiseun@gmail.com>
2024-07-03 14:15:33 +0100

    chore: Update navbar component with search bar and user profile section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { FiBell, FiChevronDown } from "react-icons/fi";
+import SearchBar from "./searchBar";
 
 function Navbar() {
-  return <div className="bg-black mt-12">Navbar</div>;
+  const profileName = "Deko";
+  //   const profilePicUrl = "https://example.com/profile-pic.jpg";
+  return (
+    <div className="bg-white py-5 px-8 relative">
+      <div className="flex text-center items-center justify-between">
+        <div className="flex flex-row gap-10 items-center">
+          <div className="text-4xl text-blue-900 font-extrabold">
+            Unlimi<span className="text-red-600">.</span>
+          </div>
+          <SearchBar />
+        </div>
+        <div className="flex items-center space-x-3">
+          <div className="relative">
+            <FiBell size={24} className="text-gray-600" />
+            <span className="absolute top-0 right-0 block h-2 w-2 rounded-full bg-red-500 ring-2 ring-white" />
+          </div>
+
+          <div className="flex items-center space-x-3">
+            <div className="h-10 w-10 rounded-full bg-black object-cover"></div>
+            <span className="font-medium text-gray-700">{profileName}</span>
+            <FiChevronDown size={20} className="text-gray-500" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
