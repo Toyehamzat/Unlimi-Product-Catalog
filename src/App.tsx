a566950e347e1d9cd1a86fb704f42ae3f491e3f5
Toyehamzat <anibiseun@gmail.com>
2024-07-04 13:34:17 +0100

    refactor: Update App component to set document title to "Unlimi"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 import ProductTable from "./components/ProductTable";
 import Navbar from "./components/navbar";
 import { ProductProvider } from "./context/productContext";
 
 function App() {
+  useEffect(() => {
+    document.title = "Unlimi";
+  }, []);
   return (
     <ProductProvider>
       <section className="m-auto bg-slate-100 h-screen max-w-full">
