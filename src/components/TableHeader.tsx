b974032ac76c9258c9bb481b535da47ced923131
Toyehamzat <anibiseun@gmail.com>
2024-07-04 12:31:33 +0100

    refactor: Update TableHeader component to fix table header widths and alignment

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -1,15 +1,11 @@
 function TableHeader() {
   const tableHeaders = [
-    { title: "S/N", width: "w-[50px] py-2" },
-    { title: "Image", width: "w-[100px] py-2" },
+    { title: "S/N", width: "w-[50px] py-2 " },
+    { title: "Image", width: "w-[100px] py-2 " },
     { title: "SKU", width: "w-[100px] py-2" },
-    { title: "Name", width: "w-[100px] py-2", className: "text-right" },
-    { title: "Title", width: "w-[400px] py-2" },
-    {
-      title: "Description",
-      width: "w-[400px] py-2",
-      className: "text-center",
-    },
+    { title: "Name", width: "w-[100px] py-2" },
+    { title: "Title", width: "w-[400px] py-2", className: "text-center" },
+    { title: "Description", width: "w-[400px] py-2", className: "text-left" },
     { title: "Brand", width: "w-[200px] py-2" },
     { title: "Cost price", width: "w-[100px] py-2" },
     { title: "Quantity", width: "w-[100px] py-2" },
@@ -19,7 +15,7 @@ function TableHeader() {
     <thead className=" px-2 py-4 bg-blue-100/55 [&_tr]:border-b">
       <tr className=" px-2 py-4 ">
         {tableHeaders.map((header) => (
-          <th key={header.title} className={header.width || header.className}>
+          <th key={header.title} className={header.width && header.className}>
             {header.title}
           </th>
         ))}
